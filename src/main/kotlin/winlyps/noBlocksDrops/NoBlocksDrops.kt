d74597092344fec044020d03aa31487fb76855c2
Winlypse <nts3@o2.pl>
2024-10-22 15:12:38 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noBlocksDrops/NoBlocksDrops.kt
@@ -0,0 +1,14 @@
+package winlyps.noBlocksDrops
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoBlocksDrops : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
