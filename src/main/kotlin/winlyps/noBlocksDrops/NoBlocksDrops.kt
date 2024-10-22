159a52a8e4de42408d56b48fa5c70d8cdf59ef09
Winlypse <nts3@o2.pl>
2024-10-22 15:12:48 +0200

    Changes!

--- src/main/kotlin/winlyps/noBlocksDrops/NoBlocksDrops.kt
+++ src/main/kotlin/winlyps/noBlocksDrops/NoBlocksDrops.kt
@@ -6,9 +6,14 @@ class NoBlocksDrops : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("doTileDrops", "false")
+        }
+        logger.info("NoBlocksDrops plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoBlocksDrops plugin has been disabled.")
     }
 }
